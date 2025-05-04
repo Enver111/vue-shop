2982bcf89c022632402dd501ade26c50b7889f37
Enver <enver.erfanovich@gmail.com>
2025-05-04 23:54:11 +0300

    Mounted cart component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+import Cart from "./components/Cart.vue";
 import Header from "./components/Header.vue";
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
+    <h2 class="text-3xl font-bold">Все товары</h2>
+
+    <div>
+      <Cart />
+    </div>
   </div>
 </template>
