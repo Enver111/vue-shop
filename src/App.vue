1b16b8f7f9d1301c00c93b82966aac37e7ce87e7
Enver <enver.erfanovich@gmail.com>
2025-05-04 23:31:10 +0300

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import Header from "./components/Header.vue";
+</script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14"></div>
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header />
+  </div>
 </template>
