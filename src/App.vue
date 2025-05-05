858e4b5144ac91edb179ff4d2e2bbf30ed93bf76
Enver <enver.erfanovich@gmail.com>
2025-05-05 22:05:31 +0300

    Mountent Drawer Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import CartList from "./components/CartList.vue";
+import Drawer from "./components/Drawer.vue";
 import Header from "./components/Header.vue";
 </script>
 
 <template>
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <div class="p-10">
