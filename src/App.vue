8ea14c14a08e429dadc7f18dba598aa40b74eaf0
Enver <enver.erfanovich@gmail.com>
2025-05-06 23:14:52 +0300

    Added a request to the server to receive goods

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
 import CartList from "./components/CartList.vue";
 import Drawer from "./components/Drawer.vue";
 import Header from "./components/Header.vue";
+
+const items = ref([]);
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      "https://58419295808a7aa4.mokky.dev/items"
+    );
+    items.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
 <template>
@@ -9,8 +23,32 @@ import Header from "./components/Header.vue";
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все товары</h2>
-      <CartList />
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все товары</h2>
+
+        <div class="flex gap-4">
+          <select
+            class="py-2 px-3 border border-gray-200 rounded-md outline-none"
+            name=""
+            id=""
+          >
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">Дорогие</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
+            <input
+              class="border border-gray-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400 transition"
+              placeholder="Поиск..."
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <CartList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,15 +1,21 @@
 <script setup>
 import Cart from "./Cart.vue";
+
+defineProps({
+  items: Array,
+});
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Cart
-      image-url="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="5000"
-      :is-added="true"
-      :isFavorite="true"
+      v-for="item in items"
+      :key="item.id"
+      :image-url="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
+      :is-added="false"
+      :isFavorite="false"
     />
   </div>
 </template>
